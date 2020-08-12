5f52d3fd18301254e5c73de8bc723da52291ae8a
Karar Al-Remahy <alremahykarar@ymail.com>
2020-08-12 02:02:49 +0200

    fix: Make sure TAKEABREAK messages have full info.

--- src/Handlers/TakeABreakHandler.ts
+++ src/Handlers/TakeABreakHandler.ts
@@ -8,11 +8,12 @@ export default class TakeABreakHandler extends DefaultHandler {
 
   // public async onCommand (msg: PrivmsgMessage) {}
 
-  public async onServerResponse ({ channelTwitch, userTwitch }: BASE) {
+  public async onServerResponse ({ channelTwitch, userTwitch, result }: BASE) {
     this.twitch.sendMessage(
       channelTwitch.name,
       userTwitch.name,
-      'take a break! You\'re currently on a cooldown period.',
+      result !== undefined && result.value.length > 0
+        ? result.value : 'take a break! You\'re currently on a cooldown period.',
     )
 
     this.twitch.removeUserInstance({ channelTwitch, userTwitch })
