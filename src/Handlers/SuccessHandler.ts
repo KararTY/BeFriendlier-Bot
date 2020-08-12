71d230968eb881d2577eb34c5b133d5363e17027
Karar Al-Remahy <alremahykarar@ymail.com>
2020-08-12 18:05:47 +0200

    feat: SuccessHandler now handles general messages.

--- src/Handlers/SuccessHandler.ts
+++ src/Handlers/SuccessHandler.ts
@@ -9,20 +9,26 @@ export default class SuccessHandler extends DefaultHandler {
   // public async onCommand (msg: PrivmsgMessage) {}
 
   public async onServerResponse ({ channelTwitch, userTwitch, result }: BASE) {
-    // Send to this user.
-    this.twitch./* TODO: whisper */sendMessage(
-      /** TODO. REMOVE */channelTwitch.name,
-      userTwitch.name,
-      String(result.value).replace('%s%', `@${String(result.matchUsername)}`),
-    )
+    // This is a match "success".
+    if (result.matchUsername !== undefined) {
+      // Send to this user.
+      this.twitch./* TODO: whisper */sendMessage(
+        /** TODO. REMOVE */channelTwitch.name,
+        userTwitch.name,
+        String(result.value).replace('%s%', `@${String(result.matchUsername)}`),
+      )
 
-    // // Send to matched user.
-    // this.twitch./* TODO: whisper */sendMessage(
-    //   /** TODO. REMOVE */channelTwitch.name,
-    //   result.matchUsername,
-    //   String(result.value).replace('%s%', `@${String(userTwitch.name)}`),
-    // )
+      // // Send to matched user.
+      // this.twitch./* TODO: whisper */sendMessage(
+      //   /** TODO. REMOVE */channelTwitch.name,
+      //   result.matchUsername,
+      //   String(result.value).replace('%s%', `@${String(userTwitch.name)}`),
+      // )
 
-    this.twitch.removeUserInstance({ channelTwitch, userTwitch })
+      this.twitch.removeUserInstance({ channelTwitch, userTwitch })
+    } else {
+      // This is a general acknowledgement message from the server.
+      this.twitch.sendMessage(channelTwitch.name, userTwitch.name, String(result.value))
+    }
   }
 }
