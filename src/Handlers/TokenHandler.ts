3b5a5305dd974229937e7e6e566fe323ffeb2e0d
Karar Al-Remahy <alremahykarar@ymail.com>
2020-08-15 14:07:22 +0200

    fix: Rejoin channels on new Token.

--- src/Handlers/TokenHandler.ts
+++ src/Handlers/TokenHandler.ts
@@ -12,6 +12,20 @@ export default class TokenHandler extends DefaultHandler {
     this.twitch.token = res
 
     // Login / Relogin to Twitch
-    return await this.twitch.loginToTwitch()
+    return await this.twitch.loginToTwitch().then(async () => {
+      if (this.twitch.channels.size > 0) {
+        // Rejoin channels
+        const channelNames: string[] = []
+        for (const [, channel] of this.twitch.channels) {
+          channelNames.push(channel.name)
+        }
+
+        await this.twitch.ircClient.joinAll(channelNames).then(() => {
+          this.logger.info(`TokenHandler.onServerResponse() -> Twitch.JOINALL: Joined ${channelNames.join(', ')}.`)
+        }).catch((error) => {
+          this.logger.error({ err: error }, 'TokenHandler.onServerResponse() -> Twitch.JOINALL')
+        })
+      }
+    })
   }
 }
