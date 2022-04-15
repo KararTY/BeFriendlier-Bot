104c97268a5a75a4f18b87d0819e3390cd93f3d5
Karar Al-Remahy <alremahykarar@ymail.com>
2022-04-16 00:56:24 +0200

    fix: No more crash on reconnect.

--- src/Handlers/ChatsHandler.ts
+++ src/Handlers/ChatsHandler.ts
@@ -8,7 +8,7 @@ export default class ChatsHandler extends DefaultHandler {
 
   // public async onCommand (msg: PrivmsgMessage) {}
 
-  public async onServerResponse (requestTime: string) {
+  public async onServerResponse (requestTime: string): Promise<void> {
     const channels = Array.from(this.twitch.channels).map(([_id, channel]) => {
       return { id: channel.id, name: channel.name }
     })
@@ -16,7 +16,12 @@ export default class ChatsHandler extends DefaultHandler {
     // If requestTime.length > 0, that means this is a requestResponse.
     const responseMessage = {
       requestTime: requestTime.length > 0 ? requestTime : undefined,
-      value: channels,
+      value: channels
+    }
+
+    if ([this.ws.client.CLOSED, this.ws.client.CLOSING, this.ws.client.CONNECTING].includes(this.ws.client.readyState as any)) {
+      this.ws.client.once('open', () => this.ws.sendMessage(this.messageType, JSON.stringify(responseMessage)))
+      return
     }
 
     this.ws.sendMessage(this.messageType, JSON.stringify(responseMessage))
