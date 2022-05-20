1389982e04d84b41f24c4db566871482d898b3de
Karar Al-Remahy <alremahykarar@ymail.com>
2022-05-20 20:12:53 +0200

    feat: another easterino eggerino

---
+++ src/Handlers/KHandler.ts
@@ -0,0 +1,16 @@
+import { PrivmsgMessage } from '@kararty/dank-twitch-irc'
+import DefaultHandler from './DefaultHandler'
+
+export default class MiscKHandler extends DefaultHandler {
+  public instantResponse = true
+
+  public prefix = ['/announce j']
+
+  public helpText = (): null => null
+
+  public async onCommand (msg: PrivmsgMessage): Promise<void> {
+    if (msg.senderUserID === '460203752' && msg.channelID === '11148817') {
+      void this.twitch.ircClient.me(msg.channelName, '/announce k')
+    }
+  }
+}
